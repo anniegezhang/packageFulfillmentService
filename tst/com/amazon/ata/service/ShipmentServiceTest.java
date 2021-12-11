9c1a57a265530276370efa13823a713f08dfcd64
Annie Zhang <gezhang84@gmail.com>
2021-12-10 20:36:44 -0800

    finished m6

--- tst/com/amazon/ata/service/ShipmentServiceTest.java
+++ tst/com/amazon/ata/service/ShipmentServiceTest.java
@@ -3,6 +3,7 @@ package com.amazon.ata.service;
 import com.amazon.ata.cost.MonetaryCostStrategy;
 import com.amazon.ata.dao.PackagingDAO;
 import com.amazon.ata.datastore.PackagingDatastore;
+import com.amazon.ata.exceptions.NoPackagingFitsItemException;
 import com.amazon.ata.types.FulfillmentCenter;
 import com.amazon.ata.types.Item;
 import com.amazon.ata.types.ShipmentOption;
@@ -49,24 +50,23 @@ class ShipmentServiceTest {
         ShipmentOption shipmentOption = shipmentService.findShipmentOption(largeItem, existentFC);
 
         // THEN
-        assertNull(shipmentOption);
+        assertNotNull(shipmentOption);
+        assertNull(shipmentOption.getPackaging());
     }
 
     @Test
     void findBestShipmentOption_nonExistentFCAndItemCanFit_returnsShipmentOption() {
-        // GIVEN & WHEN
-        ShipmentOption shipmentOption = shipmentService.findShipmentOption(smallItem, nonExistentFC);
-
-        // THEN
-        assertNull(shipmentOption);
+        // WHEN + THEN
+        assertThrows(RuntimeException.class, () -> {
+            shipmentService.findShipmentOption(smallItem, nonExistentFC);
+        }, "When FC is not exist, throw RuntimeException.");
     }
 
     @Test
     void findBestShipmentOption_nonExistentFCAndItemCannotFit_returnsShipmentOption() {
-        // GIVEN & WHEN
-        ShipmentOption shipmentOption = shipmentService.findShipmentOption(largeItem, nonExistentFC);
-
         // THEN
-        assertNull(shipmentOption);
+        assertThrows(RuntimeException.class, () -> {
+            shipmentService.findShipmentOption(largeItem, nonExistentFC);
+        }, "When FC is not exist, throw RuntimeException.");
     }
 }
